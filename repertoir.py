a17bab53d6b8b281c6e879200a306d01dece570b
Starkiller <83246502+Starkillere@users.noreply.github.com>
2021-11-20 16:42:11 +0100

    Update repertoir.py

--- repertoir.py
+++ repertoir.py
@@ -182,6 +182,12 @@ class App():
     '''class App crée une fenêtre GUI
     '''
     def __init__(self, root):
+        self.ajouter = False
+        self.rechercher = False
+        self.supprimer = False
+        self.afficherall = False
+        self.generer = False
+        self.modifier = False
         #Fenetre principale
         self.repertoir = Repertoire()
         self.text_var = StringVar()
@@ -195,10 +201,11 @@ class App():
         self.saisie = self.entry.get()
 
         self.b1 = Button(root, text ="Rechercher un contact", relief=RAISED, command=self._rechercher)
-        self.b2 = Button(root, text ="Supprimer un contact", relief=RAISED)
-        self.b3 = Button(root, text ="Modifier un contact", relief=RAISED)
-        self.b4 = Button(root, text ="Afficher tout les contacte du répertoir", relief=RAISED)
-        self.b5 = Button(root, text ="Génerer un contatct aléatoir", relief=RAISED)
+        self.b2 = Button(root, text ="Supprimer un contact", relief=RAISED,command=self._supprimer)
+        self.b3 = Button(root, text ="Modifier un contact", relief=RAISED,command=self._modifier)
+        self.b4 = Button(root, text ="Afficher tout les contacte du répertoir", relief=RAISED, command=self._afficher_all_contact)
+        self.b5 = Button(root, text ="Génerer un contatct aléatoir", relief=RAISED, command=self._generer)
+        self.b6 = Button(root, text ="Confirmer", relief=RAISED, command=self.__confirme)
 
         #coller a la fennetre
         self.label.place(x=300,y=10)
@@ -208,19 +215,52 @@ class App():
         self.b3.place(x=188,y=200)
         self.b4.place(x=100,y=250)
         self.b5.place(x=150,y=300)
-    
-    def __confirme(self, valeur=''):
-        pass
-        '''
-        self.entry.insert(END, valeur)
-        '''
-    
+        self.b6.place(x=500,y=100)
+
+    def __confirme(self,):
+        if self.rechercher:
+           self.text_var.set(self.repertoir.rechercher_contact(self.saisie))
+           self.rechercher = False
+        elif self.supprimer:
+            self.repertoir.supprimer_contact(nom=self.saisie)
+            self.supprimer = False
+        elif self.afficherall:
+            self.text_var.set(self.repertoir.afficher_repertoire())
+            self.afficherall = False
+        elif self.generer:
+            self.text_var.set(self.repertoir.randome_contact())
+            self.generer =  False
+        elif self.ajouter:
+            concernant = self.saisie.split(',')
+            self.repertoir.ajouter_contact(nom=concernant[0], prenom=concernant[1], numero=concernant[3])
+            self.ajouter = False
+        elif self.modifier:
+            a_modifier = self.saisie.split(':')
+            new = a_modifier[1].split(',')
+            self.repertoir.modifier_contact(nom=a_modifier[0], new_nom=new[0], new_prenom=new[1], new_numero=new[2])
+            self.modifier = False
     def _rechercher(self):
+        self.rechercher = True
         self.text_var.set('saisisez le nom de votre contact après avoir supprimer ce message!')
-        confirmer = Button(self.root, text ="Confirmer", relief=RAISED, command=self.__confirme(self.repertoir.rechercher_contact(self.saisie)))
-        confirmer.place(x=500,y=100)
+    
+    def _supprimer(self):
+        self.rechercher = True
+        self.text_var.set('saisissez le nom de votre contact après avoir supprimer ce message!')
+    
+    def _afficher_all_contact(self):
+        self.afficherall = True
 
+    def _generer(self):
+        self.generer = True
 
+    def _ajouter(self):
+        self.ajouter = True
+        self.text_var.set('Sausissez les information de la fçon suivante:nom,prenom,numéro.A près avoir supprimer ce message!')
+    
+    def _modifier(self):
+        self.modifier = True
+        self.text_var.set('nom:nouveau nom, nouveau prenom, nouveau numéro (les élement doivent être saisie*')
+        
     def mainloop(self):
         self.root.mainloop()
 
